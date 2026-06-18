352a0d254d864eb2b2d2d9e94bcfe10fe15acd13
stark1898y <1658608470@qq.com>
2026-06-18 15:05:37 +0800

    chore(config): 更新站点url为stark1898y.cc
    将生产环境站点地址从原github.io域名修改为自定义域名stark1898y.cc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://stark1898y.github.io',
+  url: 'https://stark1898y.cc',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
