0aaebdb02889da4183da6b168b7931042dea1f23
stark1898y <1658608470@qq.com>
2026-06-18 13:40:35 +0800

    chore(website): 更新站点基础配置信息
    修改站点标题、标语，设置正确的生产地址和GitHub仓库信息

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Stark1898y 的个人知识库',
+  tagline: '记录学习，沉淀知识',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://stark1898y.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'stark1898y', // Usually your GitHub org/user name.
+  projectName: 'stark1898y.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
