8e945b0ef661c9755f2f239404582c71299ea484
stark1898y <1658608470@qq.com>
2026-06-22 11:31:09 +0800

    chore(index): 移除首页的CSDN博客链接
    删除了首页头部区域的CSDN博客跳转按钮

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
  * @Author: stark1898y 1658608470@qq.com
  * @Date: 2026-06-22 10:42:38
  * @LastEditors: stark1898y 1658608470@qq.com
- * @LastEditTime: 2026-06-22 11:24:21
+ * @LastEditTime: 2026-06-22 11:30:56
  * @FilePath: \stark1898y.github.io\src\pages\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,12 +29,6 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro">
             开始探索 →
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            href="https://blog.csdn.net/gg1658608470?spm=1000.2115.3001.10640"
-            target="_blank">
-            CSDN 博客
           </Link>
         </div>
       </div>
